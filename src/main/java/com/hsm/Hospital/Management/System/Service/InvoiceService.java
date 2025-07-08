ff001a50486a20784919697752c3f23ac89a1a53
appie999 <aitoutmghartfarid@gmail.com>
2025-07-08 15:03:52 +0100

    add Invoice service

--- src/main/java/com/hsm/Hospital/Management/System/Service/InvoiceService.java
+++ src/main/java/com/hsm/Hospital/Management/System/Service/InvoiceService.java
@@ -1,10 +1,13 @@
 package com.hsm.Hospital.Management.System.Service;
 
 import com.hsm.Hospital.Management.System.Dto.InvoiceDTO;
+import com.hsm.Hospital.Management.System.Entity.Invoice;
 import com.hsm.Hospital.Management.System.Mapper.InvoiceMapper;
 import com.hsm.Hospital.Management.System.Repository.InvoiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InvoiceService {
@@ -17,5 +20,26 @@ public class InvoiceService {
 
     public InvoiceDTO saveInvoice(InvoiceDTO dto){
         return mapper.toDTO(repository.save(mapper.toEntity(dto)));
+    }
+
+    public InvoiceDTO getInvoiceById(Long id){
+        Invoice invoice = repository.findById(id).orElseThrow(()->new RuntimeException("invoice not found"));
+        return mapper.toDTO(invoice);
+    }
+
+    public List<InvoiceDTO> getAllInvoices(){
+        return repository.findAll().stream().map(invoice -> mapper.toDTO(invoice)).toList();
+    }
+
+    public InvoiceDTO editInvoice(Long id, InvoiceDTO dto){
+        Invoice invoice = repository.findById(id).get();
+        invoice.setId(dto.getId());
+        invoice.setDate(dto.getDate());
+        invoice.setAmount(dto.getAmount());
+        return mapper.toDTO(repository.save(invoice));
+    }
+
+    public void deleteInvoice(Long id){
+        repository.deleteById(id);
     }
 }
